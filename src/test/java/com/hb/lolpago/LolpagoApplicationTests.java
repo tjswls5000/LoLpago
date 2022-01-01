6083b189f9f35ef362597c9533304b525f95d692
Sunjin Baek <tjswls5000@gmail.com>
2022-01-01 14:15:55 +0900

    spring boot init

---
+++ src/test/java/com/hb/lolpago/LolpagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hb.lolpago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolpagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

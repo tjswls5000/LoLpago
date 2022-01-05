624e00fba791ada8ea80e0fbbb706ae79c636402
Sunjin Baek <tjswls5000@naver.com>
2022-01-05 22:38:08 +0900

    fixing

--- src/main/java/com/hb/lolpago/champions/service/ChampionsService.java
+++ src/main/java/com/hb/lolpago/champions/service/ChampionsService.java
@@ -1,13 +1,17 @@
 package com.hb.lolpago.champions.service;
 
-import java.beans.JavaBean;
+import java.util.Optional;
 
+import com.hb.lolpago.champions.dto.ChampionsDto;
 import com.hb.lolpago.champions.entity.Champions;
+import com.hb.lolpago.champions.repository.ChampionsRepository;
 import com.hb.lolpago.champions.repository.ReactiveChampionsRepository;
+import com.hb.lolpago.utlis.AppUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -15,10 +19,17 @@ import reactor.core.publisher.Mono;
 public class ChampionsService {
 
   @Autowired
-  ReactiveChampionsRepository reactiveChampionsRepository;
+  private ChampionsRepository championsRepository;
 
-  public Mono<Champions> getChampionByName(String name) {
-    return reactiveChampionsRepository.findByName(name);
+  @Autowired
+  private ReactiveChampionsRepository reactiveChampionsRepository;
+  
+  public Optional<Champions> getChampionByName(String name) {
+    return championsRepository.findByName(name);
+  }
+
+  public Mono<ChampionsDto> getChampionByNameR(String name) {
+    return reactiveChampionsRepository.findByName(name).map(AppUtils::entityToDto);
   }
 
   public Flux<Champions> getAllChampions() {
